4dc0bef8cb1129de3ff7e3d1404ac3f005a62136
doRA <dev9876dora@gmail.com>
2019-06-13 16:43:52 +0900

    [update] 初期画面
    [TODO] Unityはシングルトンダメ。

--- Assets/Lego/Scripts/Init.cs
+++ Assets/Lego/Scripts/Init.cs
@@ -1,19 +1,51 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //[TODO]
 //キャリブレーションをしない選択をした場合でも、まだキャリブレーションがされていない場合はキャリブレーションを実行する。
 public class Init : MonoBehaviour
 {
+  private LegoBase lego_ = LegoBase.Instance;
+  [SerializeField]
+  private Text text_;
+
   public void OnClickButton_yes()
   {
-    SceneManager.LoadScene("Calibration");
+    text_.text = "Calibration画面へ移行します。";
+    lego_.IsInitialized = true;
+    StartCoroutine(DelayMethod(3.5f, () =>
+    {
+      SceneManager.LoadScene("Calibration");
+    }));
   }
 
   public void OnClickButton_no()
   {
+    lego_.IsInitialized = true;
+    if (lego_.IsCalibrated)
+    {
+      text_.text = "Main画面へ移行します。";
+      StartCoroutine(DelayMethod(3.5f, () =>
+      {
+        SceneManager.LoadScene("Main");
+      }));
+    }
+    else
+    {
+      text_.text = "Calibration Dataがありません。\nCalibration画面に移行します。";
+      StartCoroutine(DelayMethod(3.5f, () =>
+      {
+        SceneManager.LoadScene("Calibration");
+      }));
+    }
+  }
 
+  private IEnumerator DelayMethod(float waitTime, Action action)
+  {
+    yield return new WaitForSeconds(waitTime);
+    action();
   }
 }
--- Assets/Lego/Scripts/LegoBase.cs
+++ Assets/Lego/Scripts/LegoBase.cs
@@ -4,24 +4,69 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-enum LegoColor
+#region Struct define
+public enum LegoColor
 {
   White, Green, Blue, Red, Yellow, YellowishGreen, Brown, Black, Orange
 }
-struct RawLegoPixelInfo
+internal struct RawLegoPixelInfo
 {
   public ushort depth;
   public Color color;
 }
 
-struct LandscapeCellInfo
+internal struct LandscapeCellInfo
 {
   public int index;
   public LegoColor legoColor;
 }
+#endregion
 
 public class LegoBase : MonoBehaviour
 {
+  #region Local Class
+  internal static class CalibrationData
+  {
+    internal static bool HasCalibrationData()
+    {
+      if (PlayerPrefs.HasKey("Init")) return true;
+      else return false;
+    }
+
+    internal static void DeleteCalibarationData()
+    {
+      PlayerPrefs.DeleteAll();
+    }
+
+    internal static void GetCalibrationData()
+    {
+      instance_.calibrationCoordinateAndDepth_[0] = new Vector3(PlayerPrefs.GetFloat("X0"), PlayerPrefs.GetFloat("Y0"), PlayerPrefs.GetFloat("D0"));
+      instance_.calibrationCoordinateAndDepth_[0] = new Vector3(PlayerPrefs.GetFloat("X1"), PlayerPrefs.GetFloat("Y1"), PlayerPrefs.GetFloat("D1"));
+      instance_.calibrationCoordinateAndDepth_[0] = new Vector3(PlayerPrefs.GetFloat("X2"), PlayerPrefs.GetFloat("Y2"), PlayerPrefs.GetFloat("D2"));
+      instance_.calibrationCoordinateAndDepth_[0] = new Vector3(PlayerPrefs.GetFloat("X3"), PlayerPrefs.GetFloat("Y3"), PlayerPrefs.GetFloat("D3"));
+    }
+
+    internal static void SetCalibrationData()
+    {
+      DeleteCalibarationData();
+      PlayerPrefs.SetInt("Init", 1);
+      PlayerPrefs.SetFloat("X0", instance_.calibrationCoordinateAndDepth_[0].x);
+      PlayerPrefs.SetFloat("X1", instance_.calibrationCoordinateAndDepth_[1].x);
+      PlayerPrefs.SetFloat("X2", instance_.calibrationCoordinateAndDepth_[2].x);
+      PlayerPrefs.SetFloat("X3", instance_.calibrationCoordinateAndDepth_[3].x);
+      PlayerPrefs.SetFloat("Y0", instance_.calibrationCoordinateAndDepth_[0].y);
+      PlayerPrefs.SetFloat("Y1", instance_.calibrationCoordinateAndDepth_[1].y);
+      PlayerPrefs.SetFloat("Y2", instance_.calibrationCoordinateAndDepth_[2].y);
+      PlayerPrefs.SetFloat("Y3", instance_.calibrationCoordinateAndDepth_[3].y);
+      PlayerPrefs.SetFloat("D0", instance_.calibrationCoordinateAndDepth_[0].z);
+      PlayerPrefs.SetFloat("D1", instance_.calibrationCoordinateAndDepth_[1].z);
+      PlayerPrefs.SetFloat("D2", instance_.calibrationCoordinateAndDepth_[2].z);
+      PlayerPrefs.SetFloat("D3", instance_.calibrationCoordinateAndDepth_[3].z);
+      PlayerPrefs.Save();
+    }
+  }
+  #endregion
+
   #region Constant Value
   private static readonly int DEPTH_CAMERA_WIDTH = 640;
   private static readonly int DEPTH_CAMERA_HEIGHT = 480;
@@ -30,69 +75,72 @@ public class LegoBase : MonoBehaviour
   public static readonly float MAX_DEPTH_NUM = 3975f;
   #endregion
 
-
-  private int calibrationDepth;
-  private static LegoBase instance_ = new LegoBase();
-  public static List<Vector3> calibrationCoordinateAndDepth_ = new List<Vector3>();
+  #region Memeber Value
   [SerializeField]
-  public static bool isCalibrated = false;
+  private RawImage colorImage_;
+  private KinectManager manager_;
+  private static LegoBase instance_ = new LegoBase();
+  private Vector3[] calibrationCoordinateAndDepth_ = new Vector3[4];
+  private float calibrationDepthAverage_;
+  private bool isCalibrated_ = false;
+  private bool isInitialized_ = false;
+  #endregion
 
-  public static LegoBase Instance{
+  #region Accessor
+  public static LegoBase Instance
+  {
     get { return instance_; }
   }
 
-  //[TODO]
-  //キャリブレーションされた値が保存されているかを確認して、ある場合は取り出す。ない場合はキャリブレーションをする。
+  public bool IsCalibrated
+  {
+    get { return isCalibrated_; }
+  }
+
+  public bool IsInitialized
+  {
+    get { return isInitialized_; }
+    set { isInitialized_ = value; }
+  }
+  #endregion
+
   protected void Start()
   {
-    /*
-    if(!PlayerPrefs.HasKey("Init")){
-      SetKey();
+    calibrationDepthAverage_ = 0;
+    if (CalibrationData.HasCalibrationData())
+    {
+      CalibrationData.GetCalibrationData();
+      instance_.isCalibrated_ = true;
+
+      for (int i = 0; i < 4; i++)
+      {
+        instance_.calibrationDepthAverage_ += instance_.calibrationCoordinateAndDepth_[i].z;
+      }
+      instance_.calibrationDepthAverage_ = instance_.calibrationDepthAverage_ / 4;
     }
-    */
-    SceneManager.LoadScene("Init");
+
+    if (!instance_.isInitialized_) SceneManager.LoadScene("Init");
   }
 
   void Update()
   {
+    manager_ = KinectManager.Instance;
 
+    if (!(manager_ && manager_.IsInitialized())) return;
+
+    if (colorImage_ && (colorImage_.texture == null))
+    {
+      colorImage_.texture = manager_.GetUsersClrTex();
+    }
   }
 
-  //[TODO]
-  //キャリブレーションした値を保存する。
-  private void StoreCalibrationValue()
+  private LegoBase() { }
+
+  RawLegoPixelInfo[,] GetTexturedata(Texture2D colorTexture)
   {
-
-  }
-
-  //外部から呼び出される。
-  public void PushCalibrationValue(List<Vector3> calibrationArray)
-  {
-    if (calibrationArray.Count < 4) return;
-
-    calibrationCoordinateAndDepth_.Add(calibrationArray[0]);
-    calibrationCoordinateAndDepth_.Add(calibrationArray[1]);
-    calibrationCoordinateAndDepth_.Add(calibrationArray[2]);
-    calibrationCoordinateAndDepth_.Add(calibrationArray[3]);
-    isCalibrated = true;
-  }
-
-  RawLegoPixelInfo[,] GetTexturedata()
-  {
-    KinectManager manager_;
-    Texture2D colorTexture_;
-    RawImage colorImage_ = null;
+    RawLegoPixelInfo[,] cameramap = new RawLegoPixelInfo[DEPTH_CAMERA_WIDTH, DEPTH_CAMERA_HEIGHT];
 
     manager_ = KinectManager.Instance;
-    colorTexture_ = new Texture2D(DEPTH_CAMERA_WIDTH, DEPTH_CAMERA_HEIGHT, TextureFormat.RGBA32, false);
-
-    Vector2 inverseY = new Vector2(1, -1);
-    colorImage_.GetComponent<Transform>().localScale *= inverseY;
-
-    colorImage_.texture = manager_.GetUsersClrTex();
-    colorTexture_ = (Texture2D)colorImage_.texture;
-
-    RawLegoPixelInfo[,] cameramap = new RawLegoPixelInfo[DEPTH_CAMERA_WIDTH, DEPTH_CAMERA_HEIGHT];
 
     for (int y = 0; y < DEPTH_CAMERA_HEIGHT; y++)
     {
@@ -101,10 +149,27 @@ public class LegoBase : MonoBehaviour
         cameramap[x, y].depth = manager_.GetDepthForPixel(x, y);
 
         Vector2 posColor = manager_.GetColorMapPosForDepthPos(new Vector2(x, y));
-        cameramap[x, y].color = colorTexture_.GetPixel((int)posColor.x, (int)posColor.y);
+        cameramap[x, y].color = colorTexture.GetPixel((int)posColor.x, (int)posColor.y);
       }
     }
 
     return cameramap;
   }
+
+  #region Function defines (Be Called by External)
+  //From Calibration.cs
+  public void PushCalibrationData(List<Vector3> calibrationArray)
+  {
+    if (calibrationArray.Count < 4) return;
+
+    instance_.calibrationCoordinateAndDepth_[0] = calibrationArray[0];
+    instance_.calibrationCoordinateAndDepth_[1] = calibrationArray[1];
+    instance_.calibrationCoordinateAndDepth_[2] = calibrationArray[2];
+    instance_.calibrationCoordinateAndDepth_[3] = calibrationArray[3];
+    instance_.isCalibrated_ = true;
+    CalibrationData.SetCalibrationData();
+  }
+  #endregion
 }
+
+
