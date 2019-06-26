9fb21c4931479924e505dbfd90f20d85c099efd2
doRA <dev9876dora@gmail.com>
2019-06-26 16:47:21 +0900

    [update] LegoObjectsで読み込まれるように修正

--- Assets/Lego/Scripts/LegoObjects.cs
+++ Assets/Lego/Scripts/LegoObjects.cs
@@ -3,20 +3,38 @@
 public static class LegoObjects
 {
   //Road
-  public static GameObject road_straight, road_intersection_T, road_intersection_X, road_curve, road_stop, road_crossWalk;
+  public static GameObject road_straight, road_intersection_T, road_intersection_X, road_curve, road_stop, road_crossWalk, road_bridge;
+
+  //Building
+  public static GameObject building_1, building_2;
 
   //Water
-  public static GameObject water_river;
+  public static GameObject water_river_straight_1, water_river_straight_2, water_river_curve;
+
+  //Nature
+  public static GameObject Nature_forest_1;
 
   public static void LoadGameObjects()
   {
+    //Road
     road_straight = (GameObject)Resources.Load("Road/Road_Straight");
     road_intersection_T = (GameObject)Resources.Load("Road/Road_Intersection_T");
     road_intersection_X = (GameObject)Resources.Load("Road/Road_Intersection_X");
     road_curve = (GameObject)Resources.Load("Road/Road_Curve");
     road_stop = (GameObject)Resources.Load("Road/Road_Stop");
     road_crossWalk = (GameObject)Resources.Load("Road/Road_Crosswalk");
+    road_bridge = (GameObject)Resources.Load("Road/Road_Bridge");
 
-    water_river = (GameObject)Resources.Load("Water/River");
+    //Building
+    building_1 = (GameObject)Resources.Load("Building/Building_1");
+    building_2 = (GameObject)Resources.Load("Building/Building_2");
+
+    //Water
+    water_river_straight_1 = (GameObject)Resources.Load("Water/Water_River_Straight_1");
+    water_river_straight_2 = (GameObject)Resources.Load("Water/Water_River_Straight_2");
+    water_river_curve = (GameObject)Resources.Load("Water/Water_River_Curve");
+
+    //Nature
+    Nature_forest_1 = (GameObject)Resources.Load("Nature/Forest_1");
   }
 }
